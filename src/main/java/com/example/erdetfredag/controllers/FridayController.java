f2519a1fe6f5fb233e15c45ded0005e509a56ac7
vital <vitaliy_serdyukov@hotmail.com>
2021-10-03 17:16:38 +0200

    @ResponseBody added

--- src/main/java/com/example/erdetfredag/controllers/FridayController.java
+++ src/main/java/com/example/erdetfredag/controllers/FridayController.java
@@ -1,11 +1,13 @@
 package com.example.erdetfredag.controllers;
 import com.example.erdetfredag.services.DayWeekCalculator;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FridayController {
   @GetMapping("/isthisfriday")
+  @ResponseBody
   public String showResult() {
     DayWeekCalculator calculator = new DayWeekCalculator();
     if (calculator.calculateDayOfWeek() == 5) {
@@ -17,6 +19,7 @@ public class FridayController {
 
 
  @GetMapping("/weekdaynumber")
+ @ResponseBody
   public String showDayNumber() {
    DayWeekCalculator calculator = new DayWeekCalculator();
 
